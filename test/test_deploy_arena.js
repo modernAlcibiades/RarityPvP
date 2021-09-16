edc5cf6fa5e4e65befaaa5528959a87fb64caabf
modernAlcibiades <mehul.kumar@columbia.edu>
2021-09-17 02:06:45 +0900

    Random number generator working. TODO - figure out how to access return value of a transaction that stores a value

--- test/test_deploy_arena.js
+++ test/test_deploy_arena.js
@@ -3,11 +3,12 @@ const { ethers } = require("hardhat");
 
 let deployer;
 let rarity, rarity_attributes, arena;
+let tokens = [
+  2371608, 2371609,
+  2383706, 2383707,]
 
 describe("Arena", function () {
   it("Deploy and test arena contract", async function () {
-    // Load account
-    let deployer;
     if (network.name == 'hardhat') {
       await hre.network.provider.request({
         method: "hardhat_impersonateAccount",
@@ -19,11 +20,16 @@ describe("Arena", function () {
     }
 
     const Arena = await ethers.getContractFactory("pvp_arena");
-    const arena = await Arena.connect(deployer).deploy();
+    arena = await Arena.connect(deployer).deploy();
     await arena.deployed();
 
     console.log("Deployed at", arena.address);
     console.log("Owner", await arena.owner());
     expect(await arena.owner()).to.equal(deployer.address);
+
+    //Create match
+    const txn = await arena.newMatch(ethers.utils.parseEther('1.0'), 3);
+    const receipt = await txn.wait();
+    console.log(receipt);
   });
 });
